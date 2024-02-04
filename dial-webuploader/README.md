193e0767548822cceeef923a618e3f154ebda012
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-02-04 12:44:04 +0900

    web uploader

---
+++ dial-webuploader/README.md
@@ -0,0 +1,1 @@
+https://lang-ship.com/blog/work/m5stickc-esp32-ota/
---
+++ dial-webuploader/src/main.cpp
@@ -0,0 +1,196 @@
+#include <Arduino.h>
+#include <ssid.h>
+
+#include <WebServer.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <M5Dial.h>
+const char *host = "m5dial";
+const char *ssid = MY_SSID;
+const char *password = MY_SSID_PASSWORD;
+WebServer server(80);
+
+/*
+ * Login page
+ */
+const char *loginIndex =
+    "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+    "<tr>"
+    "<td colspan=2>"
+    "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+    "<br>"
+    "</td>"
+    "<br>"
+    "<br>"
+    "</tr>"
+    "<td>Username:</td>"
+    "<td><input type='text' size=25 name='userid'><br></td>"
+    "</tr>"
+    "<br>"
+    "<br>"
+    "<tr>"
+    "<td>Password:</td>"
+    "<td><input type='Password' size=25 name='pwd'><br></td>"
+    "<br>"
+    "<br>"
+    "</tr>"
+    "<tr>"
+    "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+    "</tr>"
+    "</table>"
+    "</form>"
+    "<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+    "</script>";
+
+/*
+ * Server Index Page
+ */
+
+const char *serverIndex =
+    "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+    "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+    "<input type='file' name='update'>"
+    "<input type='submit' value='Update'>"
+    "</form>"
+    "<div id='prg'>progress: 0%</div>"
+    "<script>"
+    "$('form').submit(function(e){"
+    "e.preventDefault();"
+    "var form = $('#upload_form')[0];"
+    "var data = new FormData(form);"
+    " $.ajax({"
+    "url: '/update',"
+    "type: 'POST',"
+    "data: data,"
+    "contentType: false,"
+    "processData:false,"
+    "xhr: function() {"
+    "var xhr = new window.XMLHttpRequest();"
+    "xhr.upload.addEventListener('progress', function(evt) {"
+    "if (evt.lengthComputable) {"
+    "var per = evt.loaded / evt.total;"
+    "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+    "}"
+    "}, false);"
+    "return xhr;"
+    "},"
+    "success:function(d, s) {"
+    "console.log('success!')"
+    "},"
+    "error: function (a, b, c) {"
+    "}"
+    "});"
+    "});"
+    "</script>";
+
+void draw_message(const char *message)
+{
+  M5Dial.Display.fillRect(0, M5Dial.Display.height() / 2 - 10, M5Dial.Display.width(), 20, BLACK);
+  M5Dial.Display.setTextDatum(middle_center);
+  M5Dial.Display.setTextFont(&fonts::Orbitron_Light_32);
+  M5Dial.Display.setTextSize(0.5);
+  M5Dial.Display.setTextColor(ORANGE);
+  M5Dial.Display.drawString(message, M5Dial.Display.width() / 2, M5Dial.Display.height() / 2);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.write("M5Dial\n");
+  auto cfg = M5.config();
+  // cfg.serial_baudrate = 115200;
+  M5Dial.begin(cfg, true, false);
+  draw_message("Hello World");
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  draw_message("wifi begin");
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    draw_message("connect fail");
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.println(WiFi.localIP());
+  draw_message(WiFi.localIP().toString().c_str());
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host))
+  { // http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []()
+            {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex); });
+  server.on("/serverIndex", HTTP_GET, []()
+            {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex); });
+  /*handling uploading firmware file */
+  server.on(
+      "/update", HTTP_POST, []()
+      {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart(); },
+      []()
+      {
+        HTTPUpload &upload = server.upload();
+        if (upload.status == UPLOAD_FILE_START)
+        {
+          Serial.printf("Update: %s\n", upload.filename.c_str());
+          if (!Update.begin(UPDATE_SIZE_UNKNOWN))
+          { // start with max available size
+            Update.printError(Serial);
+          }
+        }
+        else if (upload.status == UPLOAD_FILE_WRITE)
+        {
+          /* flashing firmware to ESP*/
+          if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+          {
+            Update.printError(Serial);
+          }
+        }
+        else if (upload.status == UPLOAD_FILE_END)
+        {
+          if (Update.end(true))
+          { // true to set the size to the current progress
+            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+          }
+          else
+          {
+            Update.printError(Serial);
+          }
+        }
+      });
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+  delay(1);
+}
